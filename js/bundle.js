fc631ab23023c610c30748ebc6520e064a7c8413
MYefimenko <marinka0410@gmail.com>
2025-04-04 09:15:22 +0200

    Updated text

--- js/bundle.js
+++ js/bundle.js
@@ -139,10 +139,11 @@ function checkNumber() {
     const allInputs = document.querySelectorAll('.input__value'),
         result = document.querySelector('#result'),
         warning = document.querySelector('.warning');
-
+        
     // Check Number
     function checkNumber(input) {
         input.addEventListener('input', function () {
+            result.value = '';
             if (this.value.match(/\D/g)) {
                 this.style.border = '1px solid red';
                 warning.classList.add('active');
@@ -150,6 +151,7 @@ function checkNumber() {
             } else {
                 this.style.border = '1px solid #71a710';
                 warning.classList.remove('active');
+
             }
         })
     }
@@ -343,10 +345,6 @@ window.addEventListener('DOMContentLoaded', () => {
     setBtnsToLists();
     setInputToCheckbox();
     setInputToSlider();
-
-    
-
-    const div = document.querySelector('.slidecontainer');
 
 })
 })();
--- js/modules/checkNumber.js
+++ js/modules/checkNumber.js
@@ -2,10 +2,11 @@ function checkNumber() {
     const allInputs = document.querySelectorAll('.input__value'),
         result = document.querySelector('#result'),
         warning = document.querySelector('.warning');
-
+        
     // Check Number
     function checkNumber(input) {
         input.addEventListener('input', function () {
+            result.value = '';
             if (this.value.match(/\D/g)) {
                 this.style.border = '1px solid red';
                 warning.classList.add('active');
@@ -13,6 +14,7 @@ function checkNumber() {
             } else {
                 this.style.border = '1px solid #71a710';
                 warning.classList.remove('active');
+
             }
         })
     }
